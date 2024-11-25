d7f8d698be06a083d58737d994586f5e2f232c1e
noor3030 <nnorkrka@gmail.com>
2024-11-25 13:04:57 +0300

    edited files

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,9 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
     <h1 class="text-red-500">heloo</h1>
     
-    <TheWelcome />
   </main>
 </template>
