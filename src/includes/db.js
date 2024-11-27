c83bcdd308d74ecbb6537a6adc559cb071816f56
noor3030 <nnorkrka@gmail.com>
2024-11-27 11:22:28 +0300

    edit projects details

--- src/includes/db.js
+++ src/includes/db.js
@@ -1,30 +1,25 @@
-import project1 from "@/assets/images/Project 4_1.jpeg";
-import project2 from "@/assets/images/Project 5_1.jpg";
-import project3 from "@/assets/images/Project 6_1.jpg";
-import project4 from "@/assets/images/Project 6_2.jpg";
-import project5 from "@/assets/images/Project 7_1.jpg";
-import project6 from "@/assets/images/Project 7_2.jpg";
+import project1 from "@/assets/images/Project 1 & 2_3.jpg";
+import project2 from "@/assets/images/Project 3_2.jpg";
+import project3 from "@/assets/images/Project 4_1.jpeg";
+import project4 from "@/assets/images/Project 5_1.jpg";
+import project5 from "@/assets/images/Project 6_2.jpg";
+import project6 from "@/assets/images/Project 7_1.jpg";
 import project7 from "@/assets/images/Project 9_1.jpg";
 import project8 from "@/assets/images/Project 11_1.webp";
-import project9 from "@/assets/images/Project 15 & 16 & 17_2.jpeg";
-import project10 from "@/assets/images/Project 11_3.jpg";
-import project11 from "@/assets/images/Project 12_3.jpg";
-import project12 from "@/assets/images/Project 13_1.jpg";
-import project13 from "@/assets/images/Project 21_1.jpg";
+import project9 from "@/assets/images/Project 12_1.webp";
+import project10 from "@/assets/images/Project 13_2.jpg";
+import project11 from "@/assets/images/Project 15 & 16 & 17_2.jpeg";
+import project12 from "@/assets/images/Project 18_1.jpg";
+import project13 from "@/assets/images/Project 19_2.jpeg";
 import project14 from "@/assets/images/Project 20_1.jpg";
-import project15 from "@/assets/images/Project 24_1.jpg";
-import project16 from "@/assets/images/Project 12_1.webp";
-import project17 from "@/assets/images/Project 27_2.jpeg";
-import project18 from "@/assets/images/Project 19_2.jpeg";
-import project19 from "@/assets/images/Project 18_1.jpg";
-import project20 from "@/assets/images/Project 5_3.jpg";
-import project21 from "@/assets/images/Project 1 & 2_2.jpeg";
-import project22 from "@/assets/images/Project 23_1.webp";
-import project23 from "@/assets/images/Project 24_2.jpeg";
-import project24 from "@/assets/images/Project 5_2.jpg";
-import project25 from "@/assets/images/Project 26_1.webp";
-import project26 from "@/assets/images/Project 3_1.jpg";
-import project27 from "@/assets/images/Project 1 & 2_1.jpg"
+import project15 from "@/assets/images/Project 21_1.jpg";
+import project16 from "@/assets/images/Project 22_1.jpg";
+import project17 from "@/assets/images/Project 23_2.jpg";
+import project18 from "@/assets/images/Project 24_1.jpg";
+import project19 from "@/assets/images/Project 25_2.jpg";
+import project20 from "@/assets/images/banner.jpg";
+import project21 from "@/assets/images/Project 27_2.jpeg";
+
 export const projects = [
   {
     name: "Transportation of Superstructure Materials for İzmir-Çeşme Highway",
@@ -33,159 +28,123 @@ export const projects = [
     image: project1,
   },
   {
-    name: "Transportation of Superstructure Materials for İzmir-Çeşme Highway",
-    location: "İzmir-Çeşme",
-    date: 1994,
+    name: "Construction of Potable Water Supply in Kutlukent",
+    location: "Kutlukent (Samsun) İçmesuyu İnşaati",
+    date: 1997,
     image: project2,
   },
   {
-    name: "Construction of Potable Water Supply in Kutlukent (Samsun)",
-    location: "Kutlukent (Samsun)",
-    date: 1997,
+    name: "Supplementary Construction of Potable Water Supply in Büyüklü",
+    location: "Büyüklü (Samsun) İçme Suyu İkmal İnşaati",
+    date: 1999,
     image: project3,
   },
   {
-    name: "Supplementary Construction of Potable Water Supply in Büyüklü (Samsun)",
-    location: "Büyüklü (Samsun)",
-    date: 1999,
+    name: "Coastal Road Bridge Crossing Construction",
+    location: "Sahı̇ l Yolu Köprü Geçı̇ şı̇ İnşaati",
+    date: 2002,
     image: project4,
   },
   {
-    name: "Coastal Road Bridge Crossing Construction",
-    location: "Trabzon",
+    name: "Canal Medium and High Voltage Electrical Facility in Ayvacık",
+    location: "Ayvacik (Samsun) Kanal Ag-Og Elektrı̇ k Tesı̇ sı̇",
     date: 2002,
     image: project5,
   },
   {
-    name: "Canal Medium and High Voltage Electrical Facility in Ayvacık (Samsun)",
-    location: "Ayvacık (Samsun)",
-    date: 2002,
+    name: "Potable Water Reservoir Construction Project",
+    location: "İçmesuyu Depo Yapim İşı̇",
+    date: 2003,
     image: project6,
   },
   {
-    name: "Potable Water Reservoir Construction Project",
-    location: "Erzurum",
-    date: 2003,
+    name: "Road Construction through Excavation by Machinery",
+    location: "Makı̇ na İle Kazi Yapilarak Yolyapilmasi",
+    date: 2006,
     image: project7,
   },
   {
-    name: "2 Units of 500 m³ Reservoir and Maneuver Room, 2 Units of 3-Group Pumping Stations",
-    location: "Gaziantep",
-    date: 2003,
+    name: "Samsun City Center Sewerage Construction",
+    location: "Samsun Merkez Kanalı̇ zasyon",
+    date: 2005,
     image: project8,
   },
   {
-    name: "Road Construction through Excavation by Machinery",
-    location: "Konya",
+    name: "Q200, 300, 400, 500mm Concrete Pipe Sewerage Network,Reinforced Concrete Box Culvert (2.25m x 2.25m) Construction",
+    location: "Samsun Merkez Kanalı̇ zasyon",
     date: 2006,
     image: project9,
   },
   {
-    name: "Ground + 2-Storey Special Building Construction",
-    location: "Ankara",
-    date: 2006,
+    name: "Sewerage Construction in Gölkent (Sakarya)",
+    location: "Gölkent Sakarya Kanalı̇ zasyon İnşaati",
+    date: 2007,
     image: project10,
   },
   {
-    name: "Samsun City Center Sewerage Construction",
-    location: "Samsun",
-    date: 2005,
+    name: "İlkadım Sewerage Construction",
+    location: "İlkadim Kanalı̇ zasyon İnş.",
+    date: 2009,
     image: project11,
   },
   {
-    name: "Q500, 400, 300, 200mm Concrete Pipe Sewerage Network, Reinforced Concrete Box Culvert (2.25m x 2.25m) Construction",
-    location: "Samsun",
-    date: 2006,
+    name: "Atakum Sewerage Construction",
+    location: "Atakum Kanalı̇ zasyon İnş.",
+    date: 2011,
     image: project12,
   },
   {
-    name: "Sewerage Construction in Gölkent (Sakarya)",
-    location: "Gölkent (Sakarya)",
-    date: 2007,
+    name: "Atakum Balaç Creek Rehabilitation",
+    location: "Atakum Balaç Dere Islahi",
+    date: 2013,
     image: project13,
   },
   {
-    name: "Machinery and Truck Rental Services",
-    location: "Kayseri",
-    date: 2008,
+    name: "Kulu (Konya) Sewerage Construction",
+    location: "Kulu Konya Kanalı̇ zasyon İn.",
+    date: 2013,
     image: project14,
   },
   {
-    name: "İlkadım Sewerage Construction",
-    location: "İlkadım",
-    date: 2009,
+    name: "Türkeli (Sinop) Sewerage Construction",
+    location: "Türkelı̇ Sı̇ nop Kanalı̇ zasyon",
+    date: 2015,
     image: project15,
   },
   {
-    name: "İlkadım Sewerage Construction",
-    location: "İlkadım",
-    date: 2010,
+    name: "Amasya City Center Stormwater Drainage Construction",
+    location: "Amasya Merkez Yağmursuyu",
+    date: 2016,
     image: project16,
   },
   {
-    name: "İlkadım Sewerage Construction",
-    location: "İlkadım",
-    date: 2011,
+    name: "Mecitözü Potable Water Supply Construction",
+    location: "Mecı̇ tözü İçmesuyu İnş.",
+    date: 2018,
     image: project17,
   },
   {
-    name: "Atakum Sewerage Construction",
-    location: "Atakum",
-    date: 2011,
+    name: "Şendere Pond Potable Water Supply Construction",
+    location: "Şendere Göletı̇ İçm.ı̇ nş.",
+    date: 2018,
     image: project18,
   },
   {
-    name: "Atakum Balaç Creek Rehabilitation",
-    location: "Atakum Balaç",
-    date: 2013,
+    name: "Ayvacık Potable Water Supply Construction",
+    location: "Ayvacik İçmesuyu İnşaati",
+    date: 2017,
     image: project19,
   },
   {
-    name: "Kulu (Konya) Sewerage Construction",
-    location: "Kulu (Konya)",
-    date: 2013,
+    name: "Bahşılı (Kırıkkale) Sewerage Construction",
+    location: "Bahşili(Kirikkale) Kanal İnşaati",
+    date: 2020,
     image: project20,
   },
   {
-    name: "Türkeli (Sinop) Sewerage Construction",
-    location: "Türkeli (Sinop)",
-    date: 2015,
-    image: project21,
-  },
-  {
-    name: "Amasya City Center Stormwater Drainage Construction",
-    location: "Amasya",
-    date: 2016,
-    image: project22,
-  },
-  {
-    name: "Mecitözü Potable Water Supply Construction",
-    location: "Mecitözü",
-    date: 2018,
-    image: project23,
-  },
-  {
-    name: "Şendere Pond Potable Water Supply Construction",
-    location: "Şendere",
-    date: 2018,
-    image: project24,
-  },
-  {
-    name: "Ayvacık Potable Water Supply Construction",
-    location: "Ayvacık",
-    date: 2017,
-    image: project25,
-  },
-  {
-    name: "Bahşılı (Kırıkkale) Sewerage Construction",
-    location: "Bahşılı (Kırıkkale)",
-    date: 2020,
-    image: project26,
-  },
-  {
     name: "Tekkeköy District Roadworks with Tracked Excavators",
-    location: "Tekkeköy",
+    location: "Tekkeköy İlçesı̇ nde",
     date: 2020,
-    image: project27,
+    image: project21,
   },
 ];
--- src/views/GalleryView.vue
+++ src/views/GalleryView.vue
@@ -1,45 +1,54 @@
+<script setup>
+import Galleria from "primevue/galleria";
+import { projects } from "@/includes/db"; 
+
+const responsiveOptions = [
+  { breakpoint: "1024px", numVisible: 3 },
+  { breakpoint: "768px", numVisible: 2 },
+  { breakpoint: "576px", numVisible: 1 },
+];
+</script>
 <template>
-  <div class="card  flex justify-center items-center px-4 py-10 2xl:py-24 lg:px-24 2xl:px-60">
+  <div
+    class="card flex justify-center items-center px-4 py-10 2xl:py-24 lg:px-24 2xl:px-60"
+  >
     <Galleria
-        :value="projects"
-        :responsiveOptions="responsiveOptions"
-        :numVisible="5"
-        class="border-none"
+      :value="projects"
+      :responsiveOptions="responsiveOptions"
+      :numVisible="5"
+      class="border-none"
     >
       <template #item="slotProps">
         <div class="relative mb-8 w-full">
           <img
-              :src="slotProps.item.image"
-              :alt="slotProps.item.location"
-              class="w-full h-[500px] object-cover rounded-md 2xl:h-[900px]"
+            :src="slotProps.item.image"
+            :alt="slotProps.item.location"
+            class="w-full h-[500px] object-cover rounded-md 2xl:h-[900px]"
           />
-          <div class="absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 rounded-md text-white p-4">
-            <div class="md:text-2xl text-sm font-bold mb-2 2xl:mb-8 2xl:text-[40px]">{{ slotProps.item.name }}</div>
-            <p class="md:text-lg text-sm 2xl:text-[30px]">{{ slotProps.item.location }}</p>
+          <div
+            class="absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 rounded-md text-white p-4"
+          >
+            <div
+              class="md:text-2xl text-sm font-bold mb-2 2xl:mb-8 2xl:text-[40px]"
+            >
+              {{ slotProps.item.name }}
+            </div>
+            <p class="md:text-lg text-sm 2xl:text-[30px]">
+              {{ slotProps.item.location }}
+            </p>
           </div>
         </div>
       </template>
       <template #thumbnail="slotProps">
         <img
-            :src="slotProps.item.image"
-            :alt="slotProps.item.location"
-            class="block w-48 h-40 object-cover mb-8 rounded-md"
+          :src="slotProps.item.image"
+          :alt="slotProps.item.location"
+          class="block w-48 h-40 object-cover mb-8 rounded-md"
         />
       </template>
     </Galleria>
   </div>
 </template>
-
-<script setup>
-import Galleria from "primevue/galleria";
-import {projects} from "@/includes/db"; // Import the projects list
-
-const responsiveOptions = [
-  {breakpoint: "1024px", numVisible: 3},
-  {breakpoint: "768px", numVisible: 2},
-  {breakpoint: "576px", numVisible: 1},
-];
-</script>
 
 <style scoped>
 .card {
@@ -62,7 +71,6 @@ const responsiveOptions = [
   color: #fff;
 }
 </style>
-
 
 <style scoped>
 .card {
@@ -88,5 +96,4 @@ const responsiveOptions = [
 .p-galleria {
   border: none; /* Added this line to remove border */
 }
-
 </style>
